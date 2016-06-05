2cd61acf322536c73635d50bb737a7b4b217cc5d
Emiel <emielonline@gmail.com>
2016-06-05 21:19:43 +0200

    Complete website, showing different aspects of Flask.

--- Examples/Flask-website/my_first_website.py
+++ Examples/Flask-website/my_first_website.py
@@ -11,7 +11,14 @@ from flask import Flask, request, render_template
 # Other imports go here:
 from collections import Counter
 from nltk.tokenize import sent_tokenize, word_tokenize
+import csv
 
+################################################################################
+# Load data
+
+with open('./static/data/Concreteness_ratings_Brysbaert_et_al_BRM.txt') as f:
+    reader = csv.DictReader(f, delimiter='\t')
+    concreteness_values = {entry['Word']: float(entry['Conc.M']) for entry in reader}
 
 ################################################################################
 # Set up general functions
@@ -27,10 +34,18 @@ def type_token_ratio(text):
             'Tokens': num_tokens,
             'Ratio': float(num_types)/num_tokens}
 
-################################################################################
-# Load data
-
-
+def average_concreteness(text):
+    """
+    Measures the average concreteness of a text by taking the average concreteness
+    of the word forms. Probably not a very good idea!
+    """
+    words = [word for sentence in sent_tokenize(text.lower())
+                  for word in word_tokenize(sentence)
+                  if word in concreteness_values]
+    value = sum(concreteness_values[w] for w in words)/float(len(words))
+    return {'concreteness': value,
+            'words': set(words)}
+    
 
 ################################################################################
 # Set up app:
@@ -63,7 +78,25 @@ def type_token_results():
     """
     text = request.form['textfield']
     ttr_dict = type_token_ratio(text)
-    return render_template('results.html', ttr_data= ttr_dict)
+    return render_template('ttr-results.html', ttr_data= ttr_dict)
+
+
+@app.route('/concreteness/', methods=['GET'])
+def concreteness_form():
+    """
+    Display webpage with a form.
+    """
+    return render_template('concreteness.html')
+
+
+@app.route('/concreteness-results/', methods=['POST'])
+def concreteness_results():
+    """
+    Show the page with submitted information.
+    """
+    text = request.form['textfield']
+    results = average_concreteness(text)
+    return render_template('concreteness-results.html', concreteness_data= results)
 
 ################################################################################
 # Running the website
