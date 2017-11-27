df0a24c6515599007be0cfb293fdb03154e9cf61
MartenPostma <martenp@gmail.com>
2017-11-27 11:31:31 +0100

    block 4

---
+++ Chapters/the_program_v2.py
@@ -0,0 +1,15 @@
+from utils import count_words
+from utils import x
+from utils import python
+
+
+words = ['how', 'often', 'does', 'each', 'string', 'occur', 'in', 'this', 'list', '?']
+
+word2freq = count_words(words)
+print('word2freq', word2freq)
+
+
+print('x', x)
+print('python', python)
+
+
