428cb0b6e4d985d90637ff143b73d0bd61535a13
MartenPostma <martenp@gmail.com>
2019-08-29 22:08:55 +0200

    updated material block 2

---
+++ Chapters/utils_chapter11.py
@@ -0,0 +1,22 @@
+def happy_birthday(name):
+    """
+    Print a birthday song with the "name" of the person inserted.
+    """
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear " + name + ".")
+    print("Happy Birthday to you!")
+
+
+def multiply(x, y):
+    """Multiply two numeric values."""
+    result = x * y
+    print(result)
+
+
+def is_even(p):
+    """Check whether a number is even."""
+    if p % 2 == 1:
+        return False
+    else:
+        return True
