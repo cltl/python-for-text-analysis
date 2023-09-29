e8cde167bdeaa61cd28af68e703b11aa262b2982
PiaSommerauer <pia.sommerauer@live.com>
2023-09-29 12:36:32 +0200

    removing final assignment and resit assignments

--- Resit_Assignments/RESIT-A4/command_line_arguments.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-my_first_argument = sys.argv[1]
-my_second_argument = sys.argv[2]
-
-print('This is my first argument:', my_first_argument)
-print('this is my second argument:', my_second_argument)
